d075c3ff19b4ebfa8653195bb0e35f69ded99923
mars3725 <mattmohandiss@gmail.com>
2018-09-29 02:44:29 -0400

    added paging functionality

---
+++ lib/HomePage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () => Navigator.of(context).pushNamed('/OtherPage'),
+              child: Text("Next Page"),)
+          ],
+        )));
+  }
+}
---
+++ lib/OtherPage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class OtherPage extends StatefulWidget {
+  @override
+  _OtherPageState createState() => _OtherPageState();
+}
+
+class _OtherPageState extends State<OtherPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text("Next Page")
+          ],
+        )));
+  }
+}
