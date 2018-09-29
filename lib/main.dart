a573f00fb5fd812be9a4dbf76e0780e30ede46ce
bgreenb11 <bgreenb311@users.noreply.github.com>
2018-09-29 01:42:33 -0400

    BLG Check

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+
       title: 'Flutter Demo',
       theme: new ThemeData(
         // This is the theme of your application.
@@ -75,30 +76,23 @@ class _MyHomePageState extends State<MyHomePage> {
         title: new Text(widget.title),
       ),
       body: new Center(
+
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+            FlatButton(
+                onPressed: () => print('QR Pressed'),
+                child:
+                  Text('QR',style: TextStyle(fontSize: 128.0, fontWeight: FontWeight.bold), textAlign: TextAlign.center,)
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            FlatButton(
+                onPressed: () => print('NFC Pressed'),
+                child:
+                Text('NFC',style: TextStyle(fontSize: 128.0, fontWeight: FontWeight.bold),textAlign: TextAlign.center,)
             ),
           ],
         ),
